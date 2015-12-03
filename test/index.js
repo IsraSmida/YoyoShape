515fe79d572ec6cb3aac09d3c31d47123ac54468
Bob <isra.smida@gmail.com>
2015-12-03 15:17:43 +0100

    Commit test

--- test/index.js
+++ test/index.js
@@ -7,8 +7,24 @@ import yoyoShape from '../lib';
   });
 });*/
 
+var roadAttr;
+module('Unit Testing The "Shapes" Module', {
+  beforeEach: function (){
+    roadAttr = JSON.parse({building: false, highway: 'Residential', id: '-629863', nodes: [{y: 369.0, x: 708.0}, {y: 396.0, x: 743.0}], name: 'Rue de Colmar'});
+  }
+});
+
 describe('yoyoShape', function () {
   it('should have a version number!', function () {
     assert(typeof yoyoShape.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
+  });
+});
+
+describe('yoyoShape', function () {
+  it('should create default object', function () {
+    assert(typeof yoyoShape.createShape !== 'undefined', 'The Shapes module sould expose a "createShape" function');
+    var shape0 = yoyoShape.createShape(roadAttr);
+    assert(typeof shape0 === 'object', 'The "createShape" function sould return objects.');
+
   });
 });
